03897e102f4a76c70a6d2925f733ffae64f4e992
JeffordShau <jshau30@stuy.edu>
2023-05-18 17:12:46 -0400

    API template

---
+++ starter-app/db/movies.js
@@ -0,0 +1,47 @@
+const knex = require('knex');
+const config = require('../config/db.config');
+
+const db = knex(config);
+
+function getAllMovies() {
+  return db.select('*').from('movie');
+}
+
+function getMovieById(movieId) {
+  return db.select('*')
+      .from('movie')
+      .where({ id: movieId })
+      .first();
+}
+
+function createMovie(movie) {
+  movie.id = uuidv4();
+  return db.insert(movie)
+      .into('movie')
+      .returning('*');
+}
+
+function updateMovie(movie) {
+  return db('movie')
+      .where({ id: movie.id })
+      .update({
+        name: movie.name,
+        genre: movie.genre,
+        img: movie.img,
+      })
+      .returning('*');
+}
+
+function deleteMovie(movieId) {
+  return db('movie')
+      .where({ id: movieId })
+      .del();
+}
+
+module.exports = {
+  getAllMovies,
+  getMovieById,
+  createMovie,
+  updateMovie,
+  deleteMovie,
+};
